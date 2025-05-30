f6b359f630103394abb0dd21ff64cf4d2fd2119b
abikesa <144293697+abikesa@users.noreply.github.com>
2025-05-29 20:30:09 -0400

    tidy!

--- pandoc.sh
+++ pandoc.sh
@@ -1,4 +1,15 @@
-brew install pandoc  
-pandoc glp-1-prosody.md -o glp-1-prosody.html
+ 
+#!/bin/bash
+
+# brew install pandoc 
+# Update this one line only
+FILENAME="signal-cartography"
+
+# Auto-generate paths
+INPUT="../md/${FILENAME}.md"
+OUTPUT="../html/${FILENAME}.html"
+
+# Run Pandoc
+pandoc "$INPUT" -o "$OUTPUT"
 
 # insert ukubona .html stack
